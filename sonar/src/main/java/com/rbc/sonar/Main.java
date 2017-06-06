871ca4534907677b5f94460e6f698a93cc5c412b
Nijan Giree <nijan.giree@rbc.com>
2017-06-06 11:24:18 -0400

    Added sonar-project and known issue

---
+++ sonar/src/main/java/com/rbc/sonar/Main.java
@@ -0,0 +1,13 @@
+package com.rbc.sonar;
+
+/**
+ * Created by nijan on 2017-06-06.
+ */
+public class Main {
+
+    private static final String PASSWORD = "mypassword";
+
+    public static void main(String[] args) {
+        System.out.println(PASSWORD);
+    }
+}
